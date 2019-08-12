e718bfb7451dd4cdd87d5fad41686cc3bc68e170
jhejda@wmpromus.com <jhejda@wmpromus.com>
2019-08-12 16:02:33 +0200

    eyaml stuff

--- modules/demo/manifests/iis.pp
+++ modules/demo/manifests/iis.pp
@@ -261,7 +261,7 @@ class demo::iis (
     user_name                          => $service_account_username,
   }
 
-  exec { "Start application pool ${app_pool_name}":
+  -> exec { "Start application pool ${app_pool_name}":
     command   => "Start-WebAppPool -Name '${app_pool_name}'",
     provider  => 'powershell',
     onlyif    => "Import-Module WebAdministration;if((Get-ChildItem IIS:\\AppPools | where {\$_.Name -eq \'${app_pool_name}\'} | 
@@ -323,7 +323,7 @@ class demo::iis (
     require              => Iis_application_pool[$app_pool_name],
   }
 
-  exec { "Start iis_site ${iis_site_name}":
+  -> exec { "Start iis_site ${iis_site_name}":
     command  => "Start-Website '${iis_site_name}'",
     provider => 'powershell',
     onlyif   => "Import-Module WebAdministration;if((Get-ChildItem IIS:\\Sites | where {\$_.Name -eq \'${iis_site_name}\'} | 
