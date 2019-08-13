5ee1e256f639a29195b2df3e14c54aee3a2716ea
jhejda@wmpromus.com <jhejda@wmpromus.com>
2019-08-13 15:12:45 +0200

    eyaml stuff

--- modules/demo/manifests/iis.pp
+++ modules/demo/manifests/iis.pp
@@ -12,6 +12,52 @@ class demo::iis (
 
   include chocolatey
 
+  ############################################################################
+  # Create application pool                                                  #
+  ############################################################################
+
+  iis_application_pool { $app_pool_name:
+    ensure                             => 'present',
+    identity_type                      => 'NetworkService',
+    #user_name                          => $service_account_username,
+    #password                           => $service_account_password,
+    auto_start                         => true,
+    cpu_action                         => 'NoAction',
+    cpu_reset_interval                 => '00:05:00',
+    cpu_smp_affinitized                => false,
+    cpu_smp_processor_affinity_mask    => '4294967295',
+    cpu_smp_processor_affinity_mask2   => '4294967295',
+    disallow_overlapping_rotation      => false,
+    disallow_rotation_on_config_change => false,
+    enable32_bit_app_on_win64          => false,
+    enable_configuration_override      => true,
+    idle_timeout                       => '00:00:00',
+    idle_timeout_action                => 'Terminate',
+    load_balancer_capabilities         => 'HttpLevel',
+    load_user_profile                  => false,
+    log_event_on_process_model         => 'IdleTimeout',
+    log_event_on_recycle               => 'Time,Memory,OnDemand,PrivateMemory',
+    logon_type                         => 'LogonBatch',
+    managed_pipeline_mode              => 'Integrated',
+    managed_runtime_version            => 'v4.0',
+    manual_group_membership            => false,
+    max_processes                      => '1',
+    orphan_worker_process              => false,
+    pass_anonymous_token               => true,
+    ping_interval                      => '00:00:30',
+    ping_response_time                 => '00:01:30',
+    pinging_enabled                    => true,
+    queue_length                       => '1000',
+    rapid_fail_protection              => 'true',
+    rapid_fail_protection_interval     => '00:05:00',
+    rapid_fail_protection_max_crashes  => '5',
+    set_profile_environment            => true,
+    shutdown_time_limit                => '00:01:30',
+    start_mode                         => 'AlwaysRunning',
+    startup_time_limit                 => '00:01:30',
+    state                              => 'started',
+  }
+
   package { 'powershell':
     ensure   => installed,
     provider => chocolatey,
@@ -219,47 +265,7 @@ class demo::iis (
   # Create application pool and applicationpool                              #
   ############################################################################
 
-  iis_application_pool { $app_pool_name:
-    ensure                             => 'present',
-    auto_start                         => true,
-    cpu_action                         => 'NoAction',
-    cpu_reset_interval                 => '00:05:00',
-    cpu_smp_affinitized                => false,
-    cpu_smp_processor_affinity_mask    => '4294967295',
-    cpu_smp_processor_affinity_mask2   => '4294967295',
-    disallow_overlapping_rotation      => false,
-    disallow_rotation_on_config_change => false,
-    enable32_bit_app_on_win64          => false,
-    enable_configuration_override      => true,
-    identity_type                      => 'SpecificUser',
-    idle_timeout                       => '00:00:00',
-    idle_timeout_action                => 'Terminate',
-    load_balancer_capabilities         => 'HttpLevel',
-    load_user_profile                  => false,
-    log_event_on_process_model         => 'IdleTimeout',
-    log_event_on_recycle               => 'Time,Memory,OnDemand,PrivateMemory',
-    logon_type                         => 'LogonBatch',
-    managed_pipeline_mode              => 'Integrated',
-    managed_runtime_version            => 'v4.0',
-    manual_group_membership            => false,
-    max_processes                      => '1',
-    orphan_worker_process              => false,
-    pass_anonymous_token               => true,
-    password                           => $service_account_password,
-    ping_interval                      => '00:00:30',
-    ping_response_time                 => '00:01:30',
-    pinging_enabled                    => true,
-    queue_length                       => '1000',
-    rapid_fail_protection              => 'true',
-    rapid_fail_protection_interval     => '00:05:00',
-    rapid_fail_protection_max_crashes  => '5',
-    set_profile_environment            => true,
-    shutdown_time_limit                => '00:01:30',
-    start_mode                         => 'AlwaysRunning',
-    startup_time_limit                 => '00:01:30',
-    state                              => 'started',
-    user_name                          => $service_account_username,
-  }
+
 
   -> exec { "Start application pool ${app_pool_name}":
     command   => "Start-WebAppPool -Name '${app_pool_name}'",
